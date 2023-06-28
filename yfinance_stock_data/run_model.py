5d39c5ebf9d96e9b6a81e298ec0ea9d5c005fac6
kwan528 <kwan772@aucklanduni.ac.nz>
2023-06-28 18:52:38 +1200

    stock model

--- yfinance_stock_data/run_model.py
+++ yfinance_stock_data/run_model.py
@@ -14,20 +14,24 @@ if __name__ == "__main__":
     #StockData.combinePriceData(2513)
 
     process_data = Process_data()
-    # train,test, original_test = process_data.load_processed_data()
-    train,test, original_test = process_data.fetch_data()
+    train,test, original_test = process_data.load_processed_data()
+    # train,test, original_test = process_data.fetch_data()
     tensorboard_callback = TensorBoard(log_dir="./logs")
     early_stopping = tf.keras.callbacks.EarlyStopping(
         monitor='val_loss',  # The metric to monitor
         patience=3,  # Number of epochs with no improvement to wait
         mode='min'  # Training will stop when the quantity monitored has stopped decreasing
     )
+
+    # Specify GPU device
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
     # tfr.keras.losses.ListMLELoss()
     # listwise_model = RankingModel(tf.keras.losses.MeanSquaredError())
     listwise_model = RankingModel(tfr.keras.losses.ListMLELoss())
     # listwise_model.compile(optimizer=tf.keras.optimizers.Adam())
     listwise_model.compile(optimizer=tf.keras.optimizers.Adagrad(0.1))
-    listwise_model.fit(train, epochs=100, verbose=2, callbacks=[tensorboard_callback], validation_data=test)
+    listwise_model.fit(train, epochs=10, verbose=2, callbacks=[tensorboard_callback], validation_data=test)
 
     listwise_model_result = listwise_model.evaluate(test, return_dict=True)
     print("NDCG of the ListMLE model: {:.4f}".format(listwise_model_result["ndcg_metric"]))
